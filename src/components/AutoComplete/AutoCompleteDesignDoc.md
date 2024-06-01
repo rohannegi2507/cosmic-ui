8a805c6de0453f4acc63996092f28f02f94a0f2a
rohan <rohan.negi.dev@gmail.com>
2024-06-01 17:30:00 +0530

    deploy files

--- src/components/AutoComplete/AutoCompleteDesignDoc.md
+++
@@ -1,9 +0,0 @@
-#Requirments
-
-1. The component is generic enough to be usable by different websites.
-2. The input field UI and search results UI should be customizable.
-
-
-#Props
-1. options
-2. renderInput
--- src/e2e/button.test.ts
+++
@@ -1,8 +0,0 @@
-// button.test.ts
-import { test, expect } from '@playwright/test';
-
-test('button text', async ({ page }) => {
-  await page.goto('http://localhost:6006/iframe.html?id=button--primary');
-  const buttonText = await page.textContent('button');
-  expect(buttonText).toBe('Primary Button');
-});
